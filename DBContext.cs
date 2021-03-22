554ad9c0c952816a6a8415c24b08c14a4894868f
Hackerprod <hackerprodlive@gmail.com>
2021-03-22 18:52:59 +0100

    Add files via upload

---
+++ DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebTask.Entities;
+
+namespace WebTask.Database
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ProductCard> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
+
---
+++ Services/IUserService.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using WebTask;
+using WebTask.Database;
+using WebTask.Entities;
+
+namespace WebTask.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+
+    public class UserService : IUserService
+    {
+        DBContext Context;
+
+        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
+        private List<User> _users => Context.Users.ToList();
+
+
+        public UserService(DBContext context)
+        {
+            Context = context;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            var user = Context.Users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            user.Token = GenerateToken(user);
+
+            // remove password before returning
+            user.Password = null;
+
+            return user;
+        }
+
+        private string GenerateToken(User user)
+        {
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Startup.SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            // return users without passwords
+            return _users.Select(x => {
+                x.Password = null;
+                return x;
+            });
+        }
+
+        public User GetById(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.Id == id);
+
+            // return user without password
+            if (user != null)
+                user.Password = null;
+
+            return user;
+        }
+
+    }
+}
